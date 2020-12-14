a6a615e2d9baac525c2a4a0499c9c292b0c590b6
sanyabaran <75991912+sanyabaran@users.noreply.github.com>
2020-12-14 15:21:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+let todo_control = document.querySelector('.todo-control')
+let header_input = document.querySelector('.header-input')
+let todo_list = document.querySelector('.todo-list')
+let todo_completed = document.querySelector('.todo-completed')
+let todo_data 
+
+if(localStorage.todo_saved) {
+    todo_data = JSON.parse(localStorage.todo_saved)
+}
+
+else {
+    todo_data = [
+   
+    ]
+}
+
+function addToDo() {
+
+    todo_list.textContent = '';
+    todo_completed.textContent = '';
+
+    todo_data.forEach((item, i) => {
+        
+        let li = document.createElement('li')
+        li.classList.add('todo-item')
+        li.innerHTML =  `<span class="text-todo">${item.value}</span>
+        <div class="todo-buttons">
+          <button class="todo-remove"></button>
+          <button class="todo-complete"></button>
+        </div>`
+
+        let btn_todo_complete = li.querySelector('.todo-complete')
+        let btn_todo_remove = li.querySelector('.todo-remove')
+
+        btn_todo_complete.addEventListener('click', function() {
+            item.completed = !item.completed
+            addToDo()
+        })
+
+        btn_todo_remove.addEventListener('click', function() {
+            todo_data.splice(i, 1)
+            addToDo()
+        })
+
+        if(item.completed) {
+            todo_completed.prepend(li)
+        }
+        else{
+            todo_list.prepend(li)
+        }
+    });
+
+    localStorage.todo_saved = JSON.stringify(todo_data);
+}
+
+addToDo()
+
+todo_control.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    if(header_input != '') {
+       let new_todo = {
+            value: header_input.value,
+            completed: false,
+        }
+
+        header_input.value = '';
+        todo_data.push(new_todo)
+        addToDo()
+    }
+})
